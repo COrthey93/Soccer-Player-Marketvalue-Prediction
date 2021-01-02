3fb407bf4e30923ec46ab40c4ba9c58b4e24aac1
timm <timm.frischbier@freenet.de>
2021-01-02 15:43:47 +0100

    nn implementation

--- data/data_preparation.R
+++ data/data_preparation.R
@@ -1,40 +1,50 @@
-library(caret)
+# GENERAL LIBRARIES
+set.seed(80085)
 library(dplyr)
+library(ggplot2)
+
+# ML FRAMEWORK LIBRARIES
+library(caret)
+library(e1071)
+#library(doParallel)
+#registerDoParallel(makePSOCKcluster(4))
+
+# ML MODEL LIBRARIES
+library(ranger)
+library(glmnet)
 
 # 1. DATASOURCING & CLEANING
-players_21  <- read.csv("data/players_21.csv", stringsAsFactors=TRUE, encoding = "UTF-8")
-players_21  <- players_21[players_21$player_positions != "GK",]
-players_21  <- players_21[,colSums(is.na(players_21))==0]
-nums        <- unlist(lapply(players_21, is.numeric))  
-tmp         <- players_21[,nums]
+players_21  <- read.csv("../data/players_21.csv", stringsAsFactors=TRUE, encoding = "UTF-8") # Reading the initial data 
+players_21  <- players_21[players_21$player_positions != "GK",]                              # Deleting all Goalkeepers from the dataframe
+players_21  <- players_21[,colSums(is.na(players_21))==0]                                    # Substituting all NAs by Zeros
+players_21  <- players_21[players_21$value_eur != 0,]                                        # Deleting all 0 values from the market value
+nums        <- unlist(lapply(players_21, is.numeric))                                        # Identifying all numeric columns
+tmp         <- players_21[,nums]                                                             # Subsetting the data to only numeric columns
+drop        <- c("overall", "sofifa_id")                                                     # Deleting Overall (consists of single abilities) and ID (as primary key unrelated to data)
+tmp         <- tmp[,!names(tmp) %in% drop]                                                   # "
+pp_values   <- preProcess(tmp, method = c("center", "scale"))
+tmp         <- predict(pp_values, tmp)
 
-# cor_mat     <- cor(tmp)
-# cor_mat     <- round(cor_mat,2)
-# high_cor    <- caret::findCorrelation(cor_mat, cutoff = 0.8) #what does the output tell me???? how to proceed?
+#saveRDS(tmp, file = "../Player_Marketvalue_Prediction_App/ml_data.rds")
 
-control     = trainControl(method = "cv", number = 10)
-feature_selection_model <- train(value_eur~., data=tmp, method="ranger", preProcess="scale", trControl=control, importance = "impurity")
-saveRDS(object = feature_selection_model, file = "models/fs_model.rds")
-importance <- varImp(feature_selection_model, scale=FALSE)
+control_fs              <- trainControl(method = "cv", number = 10)
+feature_selection_model <- train(value_eur~., data=tmp, method="glmnet", trControl = control_fs)
+importance              <- varImp(feature_selection_model)
+importance              <- data.frame(importance[1])
+cols                    <- rownames(importance)[order(importance$Overall, decreasing = T)[1:9]]
+cols                    <- append(cols, "value_eur")
 
-# cols        <- c("value_eur", 
-#                  "age", "potential", "international_reputation", 
-#                  "overall", "skill_moves", "pace", "shooting", "passing", "dribbling", "defending", "physic")
-# df          <- tmp[cols]
+df                      <- tmp[cols]
+
+train_Index             <- createDataPartition(df$value_eur, p = .8,
+                                               list = F,
+                                               times = 1)
+train                   <- df[train_Index,]
+test                    <- df[-train_Index,]
+
+#apply(train,2,function(x) sum(is.na(x))) -> no NA values present in the train dataset. Good!
 
 # 2. BOXPLOTS
-for(i in 1:ncol(df)) {
-  boxplot(df[,i], main=names(df)[i])
-}
-
-# 3. TRAIN & TEST SPLIT
-train_Index = createDataPartition(df$international_reputation, p = .8,
-                                  list = F,
-                                  times = 1)
-train       = df[train_Index,]
-test        = df[-train_Index,]
-
-# 4. CONTROL METRIC DEFINITION
-control     = trainControl(method = "cv", number = 10)
-metric_lm   = "Rsquared"
-metric_rf   = "RMSE"
+# for(i in 1:ncol(df)) {
+#   boxplot(df[,i], main=names(df)[i])
+# }
--- models/ml_model_training.R
+++ models/ml_model_training.R
@@ -5,7 +5,8 @@ library(e1071)
 library(doParallel)
 library(ranger)
 library(ggplot2)
-set.seed(80085)
+library(neuralnet)
+#set.seed(80085)
 registerDoParallel(makePSOCKcluster(8)) # CAUTION with this line! Number of cores used may not be sufficient for your PC!
 
 # 0.  IDENTIFY VARIABLES USED FOR MODEL BUILDING
@@ -28,7 +29,42 @@ summary(rf.1)
 # 3.  LASSO/RIDGE REGRESSION MODEL
 
 # 4.  NEURAL NETWORK MODEL
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x)- min(x)))
+}
 
+
+train_Index = createDataPartition(df$international_reputation, p = .05,
+                                  list = F,
+                                  times = 1)
+traintest       = df[train_Index,]
+test        = df[-train_Index,]
+
+traintestn$value_eur <- traintest$value_eur
+traintestn$age <- normalize(traintest$age)
+
+
+tune.grid.neuralnet <- expand.grid(
+  layer1 = 256,
+  layer2 = 128,
+  layer3 = 128,
+  layer4 = 64
+)
+
+nn.1 <- caret::train(value_eur ~ age + potential + international_reputation + overall + skill_moves + pace + shooting + passing + dribbling + defending + physic, 
+                     data = traintest, 
+                     method = "neuralnet", 
+                     trControl = control , 
+                     metric = metric_rf, 
+                     stepmax = 1e6, rep = 1, 
+                     linear.output = TRUE,
+                     #algorithm = "backprop",
+                     #learningrate = 0.01,
+                     lifesign = "full", 
+                     threshold = 0.1  )
+
+getModelInfo("neuralnet")$neuralnet$parameters
+nn.1$results
 # 5.  MODEL SAVING
 saveRDS(object = rf.1, file = "models/rf_model.rds")
 
